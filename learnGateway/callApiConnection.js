17700522c78805678be67884a1afed24728bbd1d
hypermine-bc <vishwasanand.cse@gmail.com>
2019-05-26 15:14:51 +0530

    urlopen file connection

--- learnGateway/callApiConnection.js
+++ learnGateway/callApiConnection.js
@@ -7,8 +7,11 @@
 
 import { APIConnection }  from './APIConnection'
 
+const APIConn =  new APIConnection();
+
+
 // define the urlopen options
-const options = {
+const serverOptions = {
     target: 'http://127.0.0.1:42410/echo',
     // if target is https, supply a sslClientProfile
     // target: 'https://127.0.0.1:42409/echo',
@@ -20,14 +23,13 @@ const options = {
     data: "Hello DataPower GatewayScript",
 };
 
-const APIConn =  new APIConnection();
-APIConn.OpenConnectionWithServer(options)
+APIConn.OpenConnection(serverOptions)
 .then((response) => {
     if(response && response.statusCode == 200){
         response.readAsJSON((error, responseData) => {
             if (error) {
-                // error while reading response or transferring data to Buffer
-                session.output.write("readAsBuffer error: " + JSON.stringify(error));
+                // error while reading response or transferring data to JSON
+                session.output.write("readAsJSON error: " + JSON.stringify(error));
             } else {
                 session.output.write(responseData);
             } 
@@ -40,5 +42,29 @@ APIConn.OpenConnectionWithServer(options)
 });
 
 
+// define the urlopen options
+const fileOptions = {
+    // the target URL
+    target: 'local:///mylocal_directory/mylocal_file.xml',
+    // method is optional, default is GET
+    method: 'GET'
+};
 
+APIConn.OpenConnection(fileOptions)
+.then((response) => {
+    if(response && response.statusCode == 200){
+        response.readAsBuffer((error, responseData) => {
+            if (error) {
+                // error while reading response or transferring data to Buffer
+                session.output.write("read file error: " + JSON.stringify(error));
+            } else {
+                session.output.write(responseData);
+            } 
+        });
+    }
+})
+.catch((error) => {
+    console.log(error);
+    session.output.write ("urlopen connect error: " + JSON.stringify(error));
+});
 
